b541c9e9d75c2d6b3b32921a0fcde5ec48dc7c9b
Helder De Oliveira <hemarque@gmail.com>
2022-11-09 11:08:24 +0100

    playing with a set of values

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -13,3 +13,6 @@
 
 (println (mod 5 3))                                         ;playing with the order in operations
 (println (mod 3 5))
+
+(def x '(1 2 3 4))                                          ;playing with set of values
+(println (first x))
