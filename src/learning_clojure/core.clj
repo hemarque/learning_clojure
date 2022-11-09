8672644a9fbc7f853be83f7f5eeee4b14f1ccc7f
Helder De Oliveira <hemarque@gmail.com>
2022-11-09 10:48:41 +0100

    writing comments

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -1,9 +1,15 @@
 (ns learning-clojure.core)
 
+;; this is a 1 line comment
+
+(println "Hello, World!")                                   ;and this is another comment at the end of the line
+
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
 
 
-(println "Hello, World!")
+
